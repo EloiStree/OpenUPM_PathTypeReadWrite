351e2763efed873cb30dbb9b59488dfbc7d12cd5
Eloi Strée <eloistree@gmail.com>
2024-11-30 22:29:02 +0100

    Save: 11/30/2024 10:29 PM

---
+++ Runtime/Example/PathTypeMono_CreateOrReadTextFile.cs
@@ -0,0 +1,58 @@
+﻿using Eloi;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PathTypeMono_CreateOrReadTextFile : MonoBehaviour
+{
+
+    public A_PathTypeAbsoluteFileMono m_whereToStore;
+    public TextAsset m_textToWrite;
+    public bool m_executeAtAwake = true;
+
+    public UnityEvent<string> m_onTextRead;
+    public UnityEvent<string> m_onLineRead;
+
+    private void Awake()
+    {
+        if (m_executeAtAwake)
+            CreateOrReadFile();
+    }
+    public void CreateOrReadFile()
+    {
+        if (m_whereToStore != null)
+        {
+            string path = m_whereToStore.GetPath();
+            if (!File.Exists(path))
+            {
+                string text = " ";
+                if (m_textToWrite != null)
+                    text = m_textToWrite.text;
+                File.WriteAllText(path, text);
+            }
+
+            string readText = File.ReadAllText(path);
+            try
+            {
+                m_onTextRead.Invoke(readText);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(e);
+            }
+
+            string[] lines = readText.Split('\n');
+            foreach (var item in lines)
+            {
+                try
+                {
+                    m_onLineRead.Invoke(item.Trim());
+                }
+                catch (System.Exception e)
+                {
+                    Debug.LogError(e);
+                }
+            }
+        }
+    }
+}
--- Runtime/Example/PathTypeMono_OverrideTextFile.cs
+++ Runtime/Example/PathTypeMono_OverrideTextFile.cs
@@ -1,7 +1,9 @@
 using Eloi;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PathTypeMono_OverrideTextFile : MonoBehaviour
 {
@@ -9,7 +11,13 @@ public class PathTypeMono_OverrideTextFile : MonoBehaviour
     [TextArea(2, 4)]
     public string m_textToWrite = "Hello World";
     public A_PathTypeAbsoluteFileMono m_whereToStore;
+    public bool m_useAtAwake = true;
 
+    private void Awake()
+    {
+        if (m_useAtAwake)
+            WriteFile();
+    }
     [ContextMenu("Write file")]
     public void WriteFile()
     {
